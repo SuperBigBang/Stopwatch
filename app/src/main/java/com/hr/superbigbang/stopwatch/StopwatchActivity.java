09fb4e2ce3e39c6c9f21a527eee144f7d26cc81f
SuperBigBang <superbigbang@yandex.ru>
2018-09-07 14:46:46 +0900

    Initial commit

---
+++ app/src/main/java/com/hr/superbigbang/stopwatch/StopwatchActivity.java
@@ -0,0 +1,51 @@
+package com.hr.superbigbang.stopwatch;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class StopwatchActivity extends AppCompatActivity {
+    private boolean running = false;
+    private int seconds = 0;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+        runTimer();
+    }
+
+    public void onClickStartb(View view) {
+        running = true;
+    }
+
+    public void onClickStopb(View view) {
+        running = false;
+    }
+
+    public void onClickResetb(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                    System.out.println("still running");
+                } else System.out.println("waiting for start timer");
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+}
